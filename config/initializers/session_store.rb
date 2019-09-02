bb4bf0ff48001b65f1560e440dd7e0bcd0a365db
stap780 <panaet80@mail.ru>
2019-09-02 15:36:07 +0300

    change

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_myapp_session', domain: {
-    production:   '.myapp.com',
-    staging:      '.myapp.com',
+Rails.application.config.session_store :cookie_store, key: '_myappda_session', domain: {
+    production:   '.k-comment.ru',
+    staging:      '.k-comment.ru',
     development:  '.lvh.me'
 }.fetch(Rails.env.to_sym, :all)
