88740607ee694d631e4f24a34e8bf7ed7ca6bea8
stap780 <panaet80@mail.ru>
2019-09-07 19:58:06 +0300

    insint

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,15 +11,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
-    if params[:shop].present?
-      save_subdomain = "ins_"+params[:insales_id]
-      user = User.create(:name => params[:insales_id], :subdomain => save_subdomain)
-      secret_key = 'my_test_secret_key'
-      password = Digest::MD5.hexdigest(params[:token] + secret_key)
-      Insint.create(:subdomen => params[:shop],  password: password, insalesid: params[:insales_id], :user_id => user.id)
-    else
+    puts params
+    # if params[:shop].present?
+    #   save_subdomain = "ins_"+params[:insales_id]
+    #   user = User.create(:name => params[:insales_id], :subdomain => save_subdomain)
+    #   secret_key = 'my_test_secret_key'
+    #   password = Digest::MD5.hexdigest(params[:token] + secret_key)
+    #   Insint.create(:subdomen => params[:shop],  password: password, insalesid: params[:insales_id], :user_id => user.id)
+    # else
     super
-    end
+    # end
   end
 
   # GET /resource/edit
