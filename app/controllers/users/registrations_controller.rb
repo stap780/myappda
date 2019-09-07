dfd219a2007d3ad60bbe17b5f10c5700df61c8a5
stap780 <panaet80@mail.ru>
2019-09-07 21:40:52 +0300

    insint

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,15 +6,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # # GET /resource/sign_up
   def new
-    puts params
     puts params[:shop].present?
     if params[:shop].present?
-      save_subdomain = "ins_"+params[:insales_id]
-      user = User.create(:name => params[:insales_id], :subdomain => save_subdomain)
-      secret_key = 'my_test_secret_key'
-      password = Digest::MD5.hexdigest(params[:token] + secret_key)
-      Insint.create(:subdomen => params[:shop],  password: password, insalesid: params[:insales_id], :user_id => user.id)
-      head :ok  
+      save_subdomain = "insales"+params[:insales_id]
+      email = save_subdomain+"@mail.ru"
+      puts save_subdomain
+      user = User.create(:name => params[:insales_id], :subdomain => save_subdomain, :password => save_subdomain, :password_confirmation => save_subdomain, :email => email)
+      puts user.id
+      if user.id.present?
+        secret_key = 'my_test_secret_key'
+        password = Digest::MD5.hexdigest(params[:token] + secret_key)
+        Insint.create(:subdomen => params[:shop],  password: password, insalesid: params[:insales_id], :user_id => user.id)
+        head :ok
+      end
     else
     super
     end
