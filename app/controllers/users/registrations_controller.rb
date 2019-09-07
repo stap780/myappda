67c28d9b376efe1f7a50f1c1ce4f577d3629e861
stap780 <panaet80@mail.ru>
2019-09-07 20:42:04 +0300

    insint

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:new, :create]
   before_action :configure_account_update_params, only: [:update]
   before_action :redirect_to_app_url
 
