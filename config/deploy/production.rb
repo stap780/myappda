f836222e3bfa0d84aa2a9c6f4decbdf96e109a26
stap780 <panaet80@mail.ru>
2019-09-09 19:24:37 +0300

    change gemfile

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,5 +59,5 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '104.248.12.39', user: 'root', roles: %w{app db web}
+server '167.71.190.131', user: 'deploy', roles: %w{app db web}
 set :unicorn_rack_env, -> { "production" }
