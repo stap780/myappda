31d6e22341ea83d7edebc34426b78bf9265746e4
stap780 <panaet80@mail.ru>
2019-09-11 21:41:58 +0300

    change insint

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class DashboardController < ApplicationController
   def index
-    insint = Insint.find(current_user.id)
+    searchid = current_user.id
+    insint = Insint.find(searchid)
     uri = "http://k-comment:"+insint.password+"@"+insint.subdomen+"/admin/products/count.json"
     response = RestClient.get(uri)
     data = JSON.parse(response)
