d0e9ed68f54663aac22891dda752c7a993dd0e6f
stap780 <panaet80@mail.ru>
2020-01-17 17:50:44 +0300

    change

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,28 +9,26 @@ class Users::SessionsController < Devise::SessionsController
 
   # POST /resource/sign_in
   def create
-    super
       @user = User.find_by_email(params[:user][:email])
       if @user.present?
         if  @user.valid_until.nil? || @user.valid_until <= Date.today
           puts "время работы истекло - ставим плюс 1 день чтобы клиент сформировал себе счет на оплату"
           @user.update_attributes("valid_until" => Date.today)
-          sign_in(:user, @user)
+          #sign_in(:user, @user)
+          super #это проверяет логин и пароль и валидирует вход стандартными средствами и переадресовывает пользователя если надо
             if @user.subdomain == 'ketago' || @user.subdomain == 'twog'
               puts "админ вошёл - "+"#{@user.subdomain}"
-              # redirect_to after_sign_in_path_for(@user)
+             #не используем redirect_to after_sign_in_path_for(@user)
             else
               # flash[:notice] = "#{ @user.email } время работы истекло."
-              redirect_to invoice_path_for(@user), :notice => 'Оплаченный период истёк. Сервис не работает для Ваших клиентов. Пожалуйста оплатите сервис.'
+              flash[:notice] = 'Оплаченный период истёк. Сервис не работает для Ваших клиентов. Пожалуйста <a href='+"#{invoice_path_for(@user)}"+'>оплатите сервис.</a>'
+              #не используем redirect_to invoice_path_for(@user), :notice => 'Оплаченный период истёк. Сервис не работает для Ваших клиентов. Пожалуйста оплатите сервис.'
             end
         else
           puts "мы здесь"
         end
       end
   end
-  # def create
-  #   super
-  # end
 
   # DELETE /resource/sign_out
   def destroy
