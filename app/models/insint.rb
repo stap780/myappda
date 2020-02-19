5d08ada168605f73ba0e8715ea973152f5aa6eff
stap780 <panaet80@mail.ru>
2020-02-19 16:11:15 +0300

    change snippet

--- app/models/insint.rb
+++ app/models/insint.rb
@@ -219,7 +219,7 @@ def self.add_snippet(insint_id, theme_id)
     {% comment %}
       <div class="favorits-icon">
          <a href="/page/favorites">
-           <svg version='1.1' xmlns="https://www.w3.org/2000/svg" width="64px" height="60.833px" viewBox="0 0 64 60.833">
+           <svg version="1.1" xmlns="https://www.w3.org/2000/svg" width="64px" height="60.833px" viewBox="0 0 64 60.833">
                <path stroke="#000" stroke-width="5" stroke-miterlimit="10" fill-opacity="0" d="M45.684,2.654c-6.057,0-11.27,4.927-13.684,10.073 c-2.417-5.145-7.63-10.073-13.687-10.073c-8.349,0-15.125,6.776-15.125,15.127c0,16.983,17.134,21.438,28.812,38.231 c11.038-16.688,28.811-21.787,28.811-38.231C60.811,9.431,54.033,2.654,45.684,2.654z"/>
            </svg>
           <span class="js-favorts-count"></span>
