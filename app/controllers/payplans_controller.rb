73d8d01596c3a34b3f3d1f97c79558dc33a454b6
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-01-08 23:38:04 +0300

    payplans again under admin control

--- app/controllers/payplans_controller.rb
+++ app/controllers/payplans_controller.rb
@@ -1,6 +1,6 @@
 class PayplansController < ApplicationController
   before_action :authenticate_user!
-  # before_action :authenticate_admin!
+  before_action :authenticate_admin!
   before_action :set_payplan, only: [:show, :edit, :update, :destroy]
 
   # GET /payplans
