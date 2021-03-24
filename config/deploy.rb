275efcf6a4b80507a585ba366809790c3284f00b
stap780 <panaet80@mail.ru>
2021-03-24 12:35:48 +0300

    change view

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,3 +45,4 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :format, :pretty
 set :log_level, :info
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
+after 'deploy:publishing', 'unicorn:restart'
