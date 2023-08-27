2ec02257e741156cb72b3458466636084844158c
stap780 <panaet80@mail.ru>
2023-08-27 10:44:59 +0300

    add time to cases index

--- app/controllers/insints_controller.rb
+++ app/controllers/insints_controller.rb
@@ -439,12 +439,13 @@ class InsintsController < ApplicationController
 
   def extra_data
     data = {
-      "discount": +111,
+      "discount": 111,
       "discount_type": "MONEY",
       "title": "Ваша пошлина за заказ"
       }
 
-      render json: { success: true, data: data}
+      #render json: { success: true, data: data}
+      render json: data
   end
 
   private
