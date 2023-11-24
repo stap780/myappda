048251d5b4628afbe42bbbd405a4252e35599c39
stap780 <panaet80@mail.ru>
2023-11-24 17:33:59 +0300

    change to puma and change server

--- db/migrate/20190927153623_devise_add_user_date_restriction_fields.rb
+++ db/migrate/20190927153623_devise_add_user_date_restriction_fields.rb
@@ -1,4 +1,4 @@
-class DeviseAddUserDateRestrictionFields < ActiveRecord::Migration
+class DeviseAddUserDateRestrictionFields < ActiveRecord::Migration[7.0]
  def change
   add_column :users, :valid_from, :date
   add_column :users, :valid_until, :date
