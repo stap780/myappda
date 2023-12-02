01c18e3edbbc2915943e2257364ba15fade3e7cc
stap780 <panaet80@mail.ru>
2023-12-02 17:03:42 +0300

    change insint abandoned

--- app/controllers/insints_controller.rb
+++ app/controllers/insints_controller.rb
@@ -330,24 +330,31 @@ class InsintsController < ApplicationController
         client = search_client.present? ? search_client : 
                                           Client.create!( email: params["contacts"]["email"], phone: params["contacts"]["phone"], 
                                                                                               name: "abandoned_"+number.to_s)
-        mycase = Case.find_by_number(number).present? ? Case.find_by_number(number) : 
-                                                        Case.create!( number: number, casetype: 'abandoned_cart', client_id: client.id, status: "new")
+        search_mycase = Case.find_by_number(number)
+        mycase = search_mycase.present? ? search_mycase : Case.create!( number: number, casetype: 'abandoned_cart', client_id: client.id, status: "new")
+        
         puts "insint abandoned_cart mycase => "+mycase.inspect.to_s
+        
         params["lines"].each do |o_line|
           product = Product.find_by_insid(o_line["productId"].to_i).present? ? Product.find_by_insid(o_line["productId"].to_i) : 
                                                                         Product.create!(insid: o_line["productId"].to_i)
           variant = product.variants.where(insid: o_line["variantId"].to_i).present? ? product.variants.where(insid: o_line["variantId"].to_i)[0] : 
                                                                                   product.variants.create!(insid: o_line["variantId"].to_i)
+          
           # mycase.lines.create!(  product_id: product.id, variant_id: variant.id, quantity: o_line["quantity"], price: o_line["price"])
           line = mycase.lines.where(product_id: product.id, variant_id: variant.id)
-          if line.present?
-            line.first.update!(quantity: o_line["quantity"], price: o_line["full_total_price"])
-          else
-            mycase.lines.create!( product_id: product.id, variant_id: variant.id, quantity: o_line["quantity"], price: o_line["full_total_price"])
-          end
-        
+          line_data = {
+                        product_id: product.id, 
+                        variant_id: variant.id, 
+                        quantity: o_line["quantity"], 
+                        price: o_line["full_total_price"]
+                      }
+
+          line.present? ? line.first.update!(line_data) : mycase.lines.create!( line_data)
         end
-        mycase.do_event_action
+
+        mycase.do_event_action if !search_mycase.present?
+        
         render json: { success: true, message: 'Информация сохранена в cases abandoned_cart'}
       else
         render json: { error: true, message: 'не смогли добавить запись в cases abandoned_cart Сервис не включен' }
