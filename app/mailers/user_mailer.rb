5ce8b9cb90f679529bdf76308436f23cd5d104a9
stap780 <panaet80@mail.ru>
2024-03-30 10:15:32 +0300

    add email check

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,56 +3,58 @@ class UserMailer < ApplicationMailer
   before_action :set_from_email
   after_action :set_delivery_options
 
-    def test_welcome_email
-      @user_email = @user.email
-      @user_email_from = set_from_email
-      mail(to: "panaet80@gmail.com, info@two-g.ru",
-          reply_to: @user_email,
-          from:  @user_email_from,
-          subject: 'Новая регистрация в приложении myappda')
-    end
-
-    def service_end_email
-      @user_email = @user.email
-      @user_email_from = set_from_email
-      @url  = 'https://myappda.ru/users/sign_in'
-      mail(
-        to: @user_email,
+  def test_welcome_email
+    @user_email = @user.email
+    @user_email_from = set_from_email
+    mail(to: "panaet80@gmail.com, info@two-g.ru",
+        reply_to: @user_email,
         from:  @user_email_from,
-        subject: 'Заканчивается срок оплаты сервиса')
-    end
+        subject: 'Новая регистрация в приложении myappda')
+  end
 
-    def favorite_setup_service_email
-      @user_email =@user.email
-      @user_email_from = set_from_email
-      @client_count = @user.client_count
-      @url  = 'https://myappda.ru/users/sign_in'
-      mail(
-        to: @user_email,
-        from:  @user_email_from,
-        subject: 'Сервис Избранные товары не работает для ваших клиентов')
-    end
+  def service_end_email
 
-    def insales_client_api_import
-      @user_email =@user.email
-      @user_email_from = set_from_email
-      @url  = 'https://myappda.ru/users/sign_in'
-      mail(
-        to: @user_email,
-        from:  @user_email_from,
-        subject: 'Клиенты импортировались в ваш магазин')
-    end
+    @user_email = @user.email
+    @user_email_from = set_from_email
+    @url  = 'https://myappda.ru/users/sign_in'
+    mail(
+      to: @user_email,
+      from:  @user_email_from,
+      subject: 'Заканчивается срок оплаты сервиса')
+      
+  end
+
+  def favorite_setup_service_email
+    @user_email =@user.email
+    @user_email_from = set_from_email
+    @client_count = @user.client_count
+    @url  = 'https://myappda.ru/users/sign_in'
+    mail(
+      to: @user_email,
+      from:  @user_email_from,
+      subject: 'Сервис Избранные товары не работает для ваших клиентов')
+  end
+
+  def insales_client_api_import
+    @user_email =@user.email
+    @user_email_from = set_from_email
+    @url  = 'https://myappda.ru/users/sign_in'
+    mail(
+      to: @user_email,
+      from:  @user_email_from,
+      subject: 'Клиенты импортировались в ваш магазин')
+  end
 
     private
 
-    def set_from_email
-      @user && @user.has_smtp_settings? ? @user.smtp_settings[:user_name] : 'info@myappda.ru'
-    end
+  def set_from_email
+    @user && @user.has_smtp_settings? ? @user.smtp_settings[:user_name] : 'info@myappda.ru'
+  end
 
-    def set_delivery_options
-      if @user && @user.has_smtp_settings?
-        mail.delivery_method.settings.merge!(@user.smtp_settings)
-      end
+  def set_delivery_options
+    if @user && @user.has_smtp_settings?
+      mail.delivery_method.settings.merge!(@user.smtp_settings)
     end
+  end
 
 end
--- app/services/event_action_service.rb
+++ app/services/event_action_service.rb
@@ -1,15 +1,21 @@
 class EventActionService
 
-    def self.do_action(user, event, mycase)
+    def initialize(user, event, mycase)
+        @user = user
+        @event = event
+        @mycase = mycase
+    end
 
-        action = event.event_actions.first
+    def do_action
+
+        action = @event.event_actions.first
         channel = action.channel
         operation = action.operation
         pause = action.pause
         pause_time = action.pause_time
         timetable = action.timetable
         timetable_time = action.timetable_time
-        receiver = mycase.client.email if action.template.receiver == 'client'
+        receiver = @mycase.client.email if action.template.receiver == 'client'
         receiver = action.template.receiver if action.template.receiver != 'client'
         insint = user.insints.first
         wait = pause == true && pause_time.present? ? pause_time : 1
@@ -20,13 +26,14 @@ class EventActionService
         user_drop = Drops::User.new(user)
 
         if channel == 'email'
-            if mycase.casetype != 'order'
+            if @mycase.casetype != 'order'
                 case_drop = Drops::Case.new(mycase)
                 client_drop = Drops::Client.new(mycase.client)
             end
-            if mycase.casetype == 'order'
+            if @mycase.casetype == 'order'
+                check_trigger
                 service = ApiInsales.new(insint)
-                order = service.order(mycase.insales_order_id)
+                order = service.order(@mycase.insales_order_id)
                 client = service.client(order.client.id)
                 case_drop = Drops::InsalesOrder.new(order)
                 client_drop = Drops::InsalesClient.new(client)
@@ -36,29 +43,41 @@ class EventActionService
             content = content_template.render('case' => case_drop, 'client' => client_drop, 'user' => user_drop)
 
             email_data = {
-                            user: user, 
+                            user: @user, 
                             subject: subject,
                             content: content,
                             receiver: receiver
                         }
-            if mycase.casetype != 'abandoned_cart'
+            if @mycase.casetype != 'abandoned_cart' && check_trigger == true
                 EventMailer.with(email_data).send_action_email.deliver_later(wait: wait.to_i.minutes)
             end
-            if mycase.casetype == 'abandoned_cart'
-                AbandonedJob.set(wait: wait.to_i.minutes).perform_later(mycase.id, insint, email_data)
+            if @mycase.casetype == 'abandoned_cart'
+                AbandonedJob.set(wait: wait.to_i.minutes).perform_later(@mycase.id, insint, email_data)
             end
         end
 
         if channel == 'insales_api' && operation == 'cancel_order'            
-            CancelOrderJob.set(wait: wait.to_i.minutes).perform_later(mycase.insales_order_id, operation, insint)
+            CancelOrderJob.set(wait: wait.to_i.minutes).perform_later(@mycase.insales_order_id, operation, insint)
         end
 
         if channel == 'insales_api' && operation == 'preorder_order'
-            PreorderJob.set(wait: wait.to_i.minutes).perform_later(mycase.id, operation, insint)
+            PreorderJob.set(wait: wait.to_i.minutes).perform_later(@mycase.id, operation, insint)
         end
 
     end
 
+    private 
+
+    def check_trigger
+        check = false
+        if @mycase.insales_custom_status_title == @event.custom_status &&
+            @mycase.insales_financial_status == @event.financial_status
+
+            check = true
+
+        end
+        check
+    end
 end
 
 # test
