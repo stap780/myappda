6ac2a8efe1ba418659362088be3fe139f6f623de
stap780 <panaet80@mail.ru>
2024-10-19 18:21:42 +0300

    change style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -116,7 +116,7 @@ module ApplicationHelper
   end
 
   def td_tag_image(img_link = nil)
-    content_tag :td, class: "p-0 d-block", style: "min-width:70px;" do
+    content_tag :td, class: "p-0 d-block", style: "width:80px;" do
       content_tag :div, class: "img-ratio img-fit" do
         content_tag :div, class: "img-ratio__inner" do
           picture_tag([img_link], image: { class: "img-fluid img-thumbnail", loading: 'lazy' }) if img_link.present?
