6c7fd1bfa27f7478fee87e3bb1bac3a318f7ad26
stap780 <panaet80@mail.ru>
2024-10-22 16:28:48 +0300

    update worker

--- app/workers/restock_scheduler.rb
+++ app/workers/restock_scheduler.rb
@@ -1,10 +1,12 @@
 require "sidekiq-scheduler"
 
 class RestockScheduler
-  include Sidekiq::Worker
+  # include Sidekiq::Worker
+  include Sidekiq::Job
 
   def perform
     Rails.application.load_tasks
     Rake::Task["restock:check_quantity_and_send_client_email"].invoke
   end
+
 end
